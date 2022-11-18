9b7f7fcf83a5242775c6d072adba4b209981095a
Saurabh Potdar <93652523+tce-saurabhp@users.noreply.github.com>
2022-11-18 19:42:12 +0530

    Using modular imports

--- index.js
+++ index.js
@@ -1,67 +1,156 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const chromium = require('chrome-aws-lambda');
 const path = require('path');
+const fs = require('fs');
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const s3 = new S3Client({ region: "ap-south-1" });
 
-const load = async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        devtools: true,
-        //To access local files
-        args: [
-            '--disable-web-security',
-            '--disable-features=IsolateOrigins',
-            '--disable-site-isolation-trials'
-        ]
+const sourceBucketName = 'tce-predev-s3-bkt';
+const destinationBucketName = 'tce-predev-s3-bkt';
+
+const QUESTION_CLASSES = "container-fluid quiz_print one-container lower-roman hide-intro hide-answers";
+
+const SOLUTION_CLASSES = "container-fluid quiz_print one-container lower-roman hide-intro answers";
+
+const options = {
+    printBackground: true,
+    format: 'A4',
+    //format: "Letter",
+    margin: {
+        top: "20px",
+        bottom: "20px",
+        left: "20px",
+        right: "20px"
+    },
+};
+
+exports.handler = async (event) => {
+    const questionJson = JSON.parse(JSON.stringify(event)); //Create deep copy
+    questionJson["classes"] = QUESTION_CLASSES;
+    //console.log(Object.keys(questionJson));
+
+    const solutionJson = JSON.parse(JSON.stringify(event)); //Create deep copy
+    solutionJson["classes"] = SOLUTION_CLASSES;
+
+    var questionIds = questionJson['questionIds'];
+    const userId = questionJson['userId'];
+    const assignmentId = questionJson['assignmentId'];
+
+    console.log(`userId ${userId}, assignmentId ${assignmentId}`);
+
+    //Download html files for each question in parallel and save it in /tmp/ folder
+    const promiseArray = [];
+    for (let i = 0; i < questionIds.length; i++) {
+        promiseArray.push(downloadAndSaveHtml(questionIds[i]));
+    }
+    await Promise.all(promiseArray);
+
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
     });
 
-    const page = await browser.newPage();
-    
-    //Load local html file
-    await page.goto(`file:${path.join(__dirname, 'container-question.html')}`, { waitUntil: 'networkidle0' });
-    
-    //For logging inside evaluate
-    page.on('console', msg => {
-        for (let i = 0; i < msg.args().length; ++i)
-            console.log(`${i}: ${msg.args()[i]}`);
-    });
-    
-    const data = await page.evaluate(async () => {
-        const json = {
-            "question_grp": [
-                {
-                    "instruction": "( Subjective Section A )",
-                    "questions": [
-                        "qhtml/html2.html?rand=02"
-                    ]
-                },
-                {
-                    "instruction": "( Non-Subjective Section B )",
-                    "questions": [
-                        "qhtml/html1.html?rand=01",
-                        "qhtml/html3.html?rand=03",
-                        "qhtml/html4.html?rand=04",
-                        "qhtml/html5.html?rand=05",
-                        "qhtml/html6.html?rand=06",
-                        "qhtml/html7.html?rand=07",
-                        "qhtml/html8.html?rand=08",
-                        "qhtml/html9.html?rand=09"
-                    ]
-                }
-            ],
-            "classes": "container-fluid quiz_print one-container lower-roman hide-intro hide-answers",
-            "general_instructions": "<b>General Instructions</b><br><li>Answer on Plain Sheet</li><li>Number your answers correctly</li>",
-            "duaration": "10",
-            "marks": "30",
-            "subject": "Maths",
-            "grade": "Level 2",
-            "title": "Parts of the body"
-        }
-        loadQuestions(json);
-        return document.querySelector('*').outerHTML
-    });
-    
-    fs.writeFileSync('test.html', data);
+    const prefix = extractUserIdPath(userId, assignmentId);
+    const questionPdfFileName = prefix + "/" + assignmentId + "-question.pdf";
+    const solutionPdfFileName = prefix + "/" + assignmentId + "-solution.pdf";
+
+    //Create question and solution pdf parallely
+    await Promise.all([createAndUploadPdf(browser, questionJson, questionPdfFileName),
+        createAndUploadPdf(browser, solutionJson, solutionPdfFileName)
+    ]);
+
+    const questionResource = new ContentResourceVO("pdf", "question", questionPdfFileName, 0);
+    const solutionResource = new ContentResourceVO("pdf", "solution", solutionPdfFileName, 0);
+
     await browser.close();
+    return [questionResource, solutionResource];
+};
+
+/**
+ * Pass the json to container-html and get merged html.
+ * Convert the merged html to pdf
+ */
+async function createAndUploadPdf(browser, json, pdfFileName) {
+    var page = await browser.newPage();
+    await page.goto(`file:${path.join(__dirname, 'container-question.html')}`, { waitUntil: 'networkidle0' });
+    var mergedHtml = await page.evaluate(async (json) => {
+        loadQuestions(json);
+        return document.querySelector('*').outerHTML;
+    }, json);
+    //await upload("merged.html", destination, mergedHtml, 'text/html');
+
+    await page.setContent(mergedHtml, {
+        waitUntil: 'networkidle0'
+    });
+    const pdf = await page.pdf(options);
+    await upload(pdfFileName, destinationBucketName, pdf, 'application/pdf');
 }
 
-load();
+/**
+ * Download html file for the question and save it locally to /tmp/ folder
+ */
+async function downloadAndSaveHtml(questionId) {
+    let s3Path = extractQuestionPath(questionId);
+    const path = `/tmp/${questionId}.html`;
+    if (!fs.existsSync(path)) {
+        //Don't download if file already exists in /tmp/ folder
+        console.log("Downloading " + path);
+        //file exists
+        const data = await s3.send(
+            new GetObjectCommand({
+                Key: s3Path,
+                Bucket: sourceBucketName,
+            })
+        );
+        fs.writeFileSync(path, await data.Body.transformToString());
+    }
+}
+
+class ContentResourceVO {
+    constructor(fileType, type, filename, order) {
+        this.fileType = fileType;
+        this.type = type;
+        this.filename = filename;
+        this.order = order;
+    }
+}
+
+/**
+ * Extract the destination path for storing the pdf 
+ */
+function extractUserIdPath(userId, assignmentId) {
+    let path = "activity/usr/";
+    let parts = userId.split("-");
+    //Skip the first part, as it is = usr
+    for (let i = 1; i < parts.length; i++) {
+        //Get the first letter from splitted userId
+        path = path + parts[i].charAt(0) + '/';
+    }
+    return path + userId + "/print/" + assignmentId;
+}
+
+/**
+ * Extract the S3 path for questionId.html
+ */
+function extractQuestionPath(questionId) {
+    let path = "content/tqqb/";
+    const parts = questionId.split("-");
+    //Skip the first part, as it is = tqqb
+    for (let i = 1; i < parts.length; i++) {
+        //Get the first letter from each questionId
+        path = path + parts[i].charAt(0) + '/';
+    }
+    return path + questionId + "/print.html";
+}
+
+async function upload(outputFileName, destination, body, contentType) {
+    let uploadParams = {
+        Key: outputFileName,
+        Body: body,
+        Bucket: destination,
+        contentType: contentType
+    };
+    return await s3.send(new PutObjectCommand(uploadParams));
+}
